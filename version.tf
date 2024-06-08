bfccedea97981b30c77d724523d99a03c8a8eb90
Robert <roberttemta@gmail.com>
2024-06-08 12:21:47 -0500

    New version and region

--- version.tf
+++ version.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.61.0"
+      version = "4.62.0"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
